cca24d82de931cd823a5518cbb9f0e74d64684a5
teoi89 <teofil.lucaci@yahoo.ro>
2022-11-28 19:00:26 +0200

    Add project files.

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab1ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
+            daca x este divizibil cu y.
+                * In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar,
+                “indivizibil”. */
+            Console.WriteLine("Write two numbers and I`ll check if one is divisible with the other one.");
+            Console.WriteLine("Write your first number");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write your second number");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a % b == 0)
+            {
+                Console.WriteLine("Divisible");
+            }
+            else
+            {
+                Console.WriteLine("Indivisible");
+            }
+        }
+    }
+}
